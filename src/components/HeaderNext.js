acb44784da266b8dce7636713b1c65e08d81e8f0
Jason Mize <jasonemize@gmail.com>
2018-06-20 17:11:35 -0400

    api started

--- src/components/HeaderNext.js
+++ src/components/HeaderNext.js
@@ -7,11 +7,34 @@ import {
   TouchableOpacity,
 } from 'react-native';
 
+// import getAuth from '../services/FetchAuth.js';
+
 
 export default class HeaderNext extends React.Component {
   nextPageOrSubmit() {
     if (this.props.text == 'Submit >') {
       console.log('SUBMIT TO API');
+
+      fetch('https://test.salesforce.com/services/oauth2/token', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          'grant_type': 'password',
+          'client_id': 'myclientid',
+          'client_secret': 'myclientsecret',
+          'username': 'mylogin@salesforce.com',
+          'password': 'mypassword123456',
+        }),
+      }).then((response) => response.json())
+      .then((responseJson) => {
+        console.log('RESPONSE: ', responseJson);
+      });
+
+
+
     } else {
       this.props.navigation.navigate(this.props.nav_link, {
         category: this.props.category,
@@ -33,16 +56,6 @@ export default class HeaderNext extends React.Component {
         style={styles.container}
         activeOpacity={.6}
         onPress={() => this.nextPageOrSubmit()}
-        // onPress={() => this.props.navigation.navigate(this.props.nav_link, {
-        //   category: this.props.category,
-        //   location: this.props.location,
-        //   description: this.props.description,
-        //   image: this.props.image,
-        //   firstName: this.props.firstName,
-        //   lastName: this.props.lastName,
-        //   email: this.props.email,
-        //   phone: this.props.phone,
-        // })}
       >
         <View style={styles.wrap}>
 
