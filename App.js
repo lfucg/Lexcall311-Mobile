6c64b9494565cbd89bafd44a8623175ceae04c84
Jason Mize <jasonemize@gmail.com>
2018-06-27 10:27:49 -0400

    test

--- App.js
+++ App.js
@@ -22,7 +22,7 @@ const Root = createStackNavigator(
     Confirmation: ConfirmationScreen,
   },
   {
-    initialRouteName: 'Location',
+    initialRouteName: 'Home',
     navigationOptions: {
       headerStyle: {
         elevation: 1,
