9eeb577c43925a1f318c46a9d83dab3744fe3d9b
ginabethrussell <ginabeth.russell@gmail.com>
2021-05-04 17:06:12 -0400

    refactors navigationOptions as recommended in deprecation warning

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,43 +1,30 @@
-import React from 'react';
-import { 
-  Button, 
-  StyleSheet, 
-  Text, 
-  View 
-} from 'react-native';
-
+import React from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 // components
-import HeaderTitle from './HeaderTitle.js';
-import HeaderBack from './HeaderBack.js';
-import HeaderNext from './HeaderNext.js';
-import MenuOption from './MenuOption.js';
-import IndexCreateReport from './IndexCreateReport.js';
-import HeaderImg from './HeaderImg.js';
+import HeaderTitle from "./HeaderTitle.js";
+import HeaderBack from "./HeaderBack.js";
+import HeaderNext from "./HeaderNext.js";
+import MenuOption from "./MenuOption.js";
+import IndexCreateReport from "./IndexCreateReport.js";
+import HeaderImg from "./HeaderImg.js";
 
 // images
-import search_img from '../assets/images/icon_search.png';
-import phone_img from '../assets/images/icon_phone.png';
-import car_img from '../assets/images/icon_car.png';
-import external_link_alt_img from '../assets/images/icon_external-link-alt.png';
-import bell_img from '../assets/images/icon_bell.png';
-import map_img from '../assets/images/icon_map.png';
+import search_img from "../assets/images/icon_search.png";
+import phone_img from "../assets/images/icon_phone.png";
+import car_img from "../assets/images/icon_car.png";
+import external_link_alt_img from "../assets/images/icon_external-link-alt.png";
+import bell_img from "../assets/images/icon_bell.png";
+import map_img from "../assets/images/icon_map.png";
 
 export default class HomeScreen extends React.Component {
-
- static navigationOptions = ({navigation}) => {
+  static navigationOptions = ({ navigation }) => {
     return {
-      headerLeft: (
-        <HeaderBack />
-      ),
-      headerTitle: (
-        <HeaderTitle text={"LexCall 311"}/>
-      ),
-      headerRight: (
-        <HeaderNext />
-      ),
+      headerLeft: () => <HeaderBack />,
+      headerTitle: () => <HeaderTitle text={"LexCall 311"} />,
+      headerRight: () => <HeaderNext />,
     };
-  }
+  };
 
   render() {
     return (
@@ -47,42 +34,42 @@ export default class HomeScreen extends React.Component {
         </View>
         <View style={styles.menu}>
           {
-          <MenuOption
-            navigation={this.props.navigation}
-            nav_link={'Phone'} 
-            img={phone_img} 
-            text={"Call LexCall 311"} 
-          />
+            <MenuOption
+              navigation={this.props.navigation}
+              nav_link={"Phone"}
+              img={phone_img}
+              text={"Call LexCall 311"}
+            />
           }
           <MenuOption
             navigation={this.props.navigation}
-            nav_link={'SignUp'} 
-            img={bell_img} 
-            text={"Sign up for 311 Alerts"} 
+            nav_link={"SignUp"}
+            img={bell_img}
+            text={"Sign up for 311 Alerts"}
           />
           <MenuOption
             navigation={this.props.navigation}
-            nav_link={'WebReport'} 
-            img={search_img} 
-            text={"Look up a 311 Report"} 
+            nav_link={"WebReport"}
+            img={search_img}
+            text={"Look up a 311 Report"}
           />
           <MenuOption
             navigation={this.props.navigation}
-            nav_link={'MapOfReports'} 
-            img={map_img} 
-            text={"View Map of Current Reports"} 
+            nav_link={"MapOfReports"}
+            img={map_img}
+            text={"View Map of Current Reports"}
           />
           <MenuOption
             navigation={this.props.navigation}
-            nav_link={'TrafficInfo'} 
-            img={car_img} 
-            text={"Traffic Info"} 
+            nav_link={"TrafficInfo"}
+            img={car_img}
+            text={"Traffic Info"}
           />
           <MenuOption
             navigation={this.props.navigation}
-            nav_link={'LexingtonWebsite'} 
-            img={external_link_alt_img} 
-            text={"Visit lexingtonky.gov"} 
+            nav_link={"LexingtonWebsite"}
+            img={external_link_alt_img}
+            text={"Visit lexingtonky.gov"}
           />
           <IndexCreateReport navigation={this.props.navigation} />
         </View>
@@ -94,7 +81,7 @@ export default class HomeScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   menu: {
     flex: 5,
@@ -103,4 +90,3 @@ const styles = StyleSheet.create({
     flex: 4,
   },
 });
-
