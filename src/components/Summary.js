fe328d0a493b8b7b7760c39041abc7a9e7d587ec
ginabethrussell <ginabeth.russell@gmail.com>
2021-05-04 17:01:45 -0400

    wraps createStackNavigator with createAppContainer

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,53 +1,38 @@
-import React from 'react';
-import { 
-  StyleSheet, 
-  Text, 
-  View, 
-  Image, 
-  Platform, 
-} from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Image, Platform } from "react-native";
 
 export default class Summary extends React.Component {
-
   render() {
-
     return (
       <View style={styles.container}>
         <View style={styles.wrap}>
           <View style={styles.img_wrap}>
-            <Image
-              source={this.props.icon}
-              style={styles.icon}
-            />
+            <Image source={this.props.icon} style={styles.icon} />
           </View>
-          
-          <Text style={styles.heading}>
-            {this.props.heading}
-          </Text>
-          <Text style={styles.content}>
-            {this.props.content}
-          </Text>
+
+          <Text style={styles.heading}>{this.props.heading}</Text>
+          <Text style={styles.content}>{this.props.content}</Text>
         </View>
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     paddingLeft: 10,
     paddingRight: 10,
   },
   wrap: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   img_wrap: {
     height: 30,
     marginTop: 10,
     marginBottom: 5,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   icon: {
     width: 30,
@@ -55,15 +40,14 @@ const styles = StyleSheet.create({
   },
   heading: {
     height: 30,
-    color: '#000',
+    color: "#000",
     fontSize: 20,
-    fontWeight: '600',
+    fontWeight: "600",
   },
   content: {
     fontSize: 16,
     paddingBottom: 10,
-    color: '#585858',
-    textAlign: 'center',
+    color: "#585858",
+    textAlign: "center",
   },
-})
-
+});
