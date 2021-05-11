88355cf55a81f2f0f167f604cf703e9e4e9ad01e
ginabethrussell <ginabeth.russell@gmail.com>
2021-05-11 08:31:16 -0400

    adds icons to category screen

--- src/components/CategoryScreen.js
+++ src/components/CategoryScreen.js
@@ -22,6 +22,8 @@ import recycle_img from "../assets/images/icon_recycle.png";
 import sign_img from "../assets/images/icon_map-signs.png";
 import tree_img from "../assets/images/icon_tree.png";
 import ellipsis_img from "../assets/images/icon_ellipsis-h.png";
+import house_img from "../assets/images/icon_house.png";
+import sidewalk_img from "../assets/images/icon_sidewalk.png";
 
 export default class CategoryScreen extends React.Component {
   static navigationOptions = ({ navigation }) => {
@@ -59,7 +61,7 @@ export default class CategoryScreen extends React.Component {
             navigation={this.props.navigation}
             nav_link={"Location"}
             img={trash_img}
-            text={"Appliance / Tires"}
+            text={"Appliance/Tires"}
             category={"trash_pickup"}
           />
           <MenuOption
@@ -114,14 +116,14 @@ export default class CategoryScreen extends React.Component {
           <MenuOption
             navigation={this.props.navigation}
             nav_link={"Location"}
-            img={sign_img}
+            img={house_img}
             text={"Housing Complaint/Non-Emergency"}
             category={"housing_complaint"}
           />
           <MenuOption
             navigation={this.props.navigation}
             nav_link={"Location"}
-            img={sign_img}
+            img={sidewalk_img}
             text={"Sidewalk Complaint"}
             category={"sidewalk_complaint"}
           />
