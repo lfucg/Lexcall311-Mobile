eba8778d4f341aa34c3a6931218330a00388d0f0
Jason Mize <jasonemize@gmail.com>
2023-02-10 13:53:52 -0500

    deploy 3.0.3

--- config/version.js
+++ config/version.js
@@ -1,3 +1,3 @@
-export const version = '3.0.2'
-export const buildNumber = '3.0.2'
-export const versionCode = '10'
+export const version = '3.0.3'
+export const buildNumber = '3.0.3'
+export const versionCode = '11'
